afd7fe971acbb9463b0a027f736985801ead86fe
candland <candland@gmail.com>
2015-08-28 10:21:45 -0600

    Removed snapshot so lein install works.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject rox-cli/lein-template "0.1.0-SNAPSHOT"
+(defproject rox-cli/lein-template "0.1.0"
   :description "Template for creating CLI applications."
   :url "http://roximity.com"
   :license {:name "Eclipse Public License"
